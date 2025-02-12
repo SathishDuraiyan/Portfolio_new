113f9a4fa5f72c00bbe80049e99f7151f79be40b
Sathish-Duraiyan <2k21cse132@kiot.ac.in>
2025-02-12 09:38:00 +0530

    Tailwind Working

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Button } from './Components/Button';
+import React from 'react'
+import { Button } from './Components/Button'
+
 const App = () => {
   return (
-    <>
-    <Button>Button</Button>
-    </>
+    <div className="">
+      <Button>Button</Button>
+    </div>
   )
 }
 
