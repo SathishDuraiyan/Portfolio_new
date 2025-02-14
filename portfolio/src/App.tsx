9895bd1296e95d8eb6381f88fb2955cfc4533be8
Sathish-Duraiyan <2k21cse132@kiot.ac.in>
2025-02-14 06:36:44 +0530

    About Page Updated

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react'
 import Home from './UI/Home'
+import About from "./UI/About";
 const App = () => {
   return (
     <Router>
       <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About/>}></Route>
       </Routes>
     </Router>
   )
--- portfolio/src/UI/Home.tsx
+++ portfolio/src/UI/Home.tsx
@@ -7,30 +7,32 @@ import { FaLinkedin } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 import { Button } from "../Components/Button";
 import Skills from "./Skills";
+import Experience from "./Experience";
+// import ProjectList from "./ProjectList";
+import Footer from "./Footer";
 const Home = () => {
   return (
-    <div className="bg-black h-screen w-full ">
+    <div className="bg-[#111111] h-full w-full ">
       <Navbar />
-      <div className="py-20 px-38">
+      <div className="flex flex-col mt-10 justify-center md:px-[15%] space-y-6 px-[7%]">
         <div className="p-8 border-2 border-[#222222] rounded-2xl flex flex-col gap-4 text-white">
             
               <p className="text-4xl font-bold">
               Sathish Duraiyan !
               </p>
-          
-
+        
             <div className="flex flex-row gap-5 text-white justify-between ">
               <div className="flex flex-row gap-3 items-center">
                 <FaLocationDot></FaLocationDot>
                 <p className="text-slate-400">
-                  Salem - 637502, Tamil Nadu, India
+                  Salem - 637 502, Tamil Nadu, India
                 </p>
               </div>
               <div className="flex flex-row gap-5 text-white">
-                <IoLogoGithub className="w-7 h-7"></IoLogoGithub>
-                <SiLeetcode className="w-7 h-7"></SiLeetcode>
-                <SiGmail className="w-7 h-7"></SiGmail>
-                <FaLinkedin className="w-7 h-7"></FaLinkedin>
+                <a href="https://github.com/SathishDuraiyan" target="_blank"><IoLogoGithub className="border-2 border-[#27272a] p-1.5 rounded-md cursor-pointer hover:shadow-red-600 hover:shadow-2xl w-10 h-10"/></a>
+                <a href="https://leetcode.com/u/Sathish_Duraiyan/" target="_blank"><SiLeetcode className="border-2 border-[#27272a] p-1.5 rounded-md cursor-pointer hover:shadow-red-600 hover:shadow-2xl w-10 h-10"/></a>
+                <a href="mailto:sathishduriyan6@gmail.com" target="_blank"><SiGmail className="border-2 border-[#27272a] p-1.5 rounded-md cursor-pointer hover:shadow-red-600  hover:shadow-2xl w-10 h-10"/></a>
+                <a href="https://www.linkedin.com/in/sathish-duraiyan/" target="_blank"> <FaLinkedin className="border-2 border-[#27272a] p-1.5 rounded-md cursor-pointer hover:shadow-red-600 hover:shadow-2xl w-10 h-10"/></a>
               </div>
             </div>
 
@@ -38,7 +40,7 @@ const Home = () => {
               <p className="text-white text-[18px] flex flex-col">
                 I am Java Full Stack Developer and UI/UX Designer, Currently
                 pursuving B.E., Final Year Computer Science and Engineering.
-                <br></br>I excel at problem-solving, quick learning, and
+                I excel at problem-solving, quick learning, and
                 researching to develop real-world solutions with creativity and
                 logic.
               </p>
@@ -49,6 +51,10 @@ const Home = () => {
         </div>
         <div className="pt-5">
         <Skills></Skills>
+        <Experience></Experience>
+        {/* <ProjectList></ProjectList> */}
+        <div className="w-full border border-gray-700/40 mb-10"></div>
+        <Footer></Footer>
         </div>
         
       </div>
